0dca6c720fdbff9d69c41e0a767b93c62536db97
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-11-27 11:18:06 +0100

    Remove the configuration to avoid any security concerns.

--- src/Controller/Homepage.php
+++ src/Controller/Homepage.php
@@ -4,28 +4,22 @@ declare(strict_types=1);
 
 namespace drupol\CasBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class Homepage.
  */
-final class Homepage extends AbstractController
+final class Homepage
 {
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function __invoke()
     {
-        $configuration = print_r($this->container->get('parameter_bag')->get('cas'), true);
-
-        $body = <<<EOF
+        $body = <<< 'EOF'
 <p>You have been redirected here by default. You are most probably using the default CAS configuration.</p>
 
-<p>Here's a dump of the current enabled configuration:</p>
-<textarea>$configuration</textarea>
-
-<p>The default CAS bundle configuration is installed in <code>config/packages/dev/cas_bundle.yaml</code></p>
+<p>The default CAS bundle configuration should be installed in <code>config/packages/dev/cas_bundle.yaml</code></p>
 
 <p>Please update your configuration and replace <code>cas_bundle_homepage</code> with an existing route of your app.</p>
 EOF;
