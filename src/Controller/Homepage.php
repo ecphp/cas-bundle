cb0f7e9ecbe7c91f3531336b718333dff212d34f
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-11-27 11:15:51 +0100

    Update default CAS homepage with more information to the end user.

--- src/Controller/Homepage.php
+++ src/Controller/Homepage.php
@@ -4,20 +4,28 @@ declare(strict_types=1);
 
 namespace drupol\CasBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class Homepage.
  */
-final class Homepage
+final class Homepage extends AbstractController
 {
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function __invoke()
     {
-        $body = <<< 'EOF'
-<p>You have been redirected here by default.</p>
+        $configuration = print_r($this->container->get('parameter_bag')->get('cas'), true);
+
+        $body = <<<EOF
+<p>You have been redirected here by default. You are most probably using the default CAS configuration.</p>
+
+<p>Here's a dump of the current enabled configuration:</p>
+<textarea>$configuration</textarea>
+
+<p>The default CAS bundle configuration is installed in <code>config/packages/dev/cas_bundle.yaml</code></p>
 
 <p>Please update your configuration and replace <code>cas_bundle_homepage</code> with an existing route of your app.</p>
 EOF;
