4ae3502794e3828df697205074403b128d54346d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-11-28 08:45:34 +0100

    Add tests.

--- src/DependencyInjection/CasExtension.php
+++ src/DependencyInjection/CasExtension.php
@@ -8,9 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
- * This is the class that loads and manages your bundle configuration.
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * Class CasExtension.
  */
 class CasExtension extends Extension
 {
