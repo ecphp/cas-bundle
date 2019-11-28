462266673500acd62a30647b760da56ca7778842
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-11-28 12:19:32 +0100

    Sync from branch 4.4

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -8,9 +8,7 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files.
- *
- * @see http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class
+ * Class Configuration.
  */
 class Configuration implements ConfigurationInterface
 {
