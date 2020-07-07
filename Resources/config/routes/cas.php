c28377bffed12d5c998f7d565c727af856fddf90
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-07-07 11:53:53 +0200

    Revert "Replace user routing file cas.yaml with cas.php."
    This reverts commit 5321218d

--- Resources/config/routes/cas.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-
-return static function (RoutingConfigurator $routes) {
-    $routes
-        ->import('@CasBundle/Resources/config/routes/routes.php')
-        ->prefix('/cas');
-};
