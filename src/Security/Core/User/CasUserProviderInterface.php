6a3ee9aa10b0a3f5a0e02b5e55138cf73b14c144
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-07-27 15:08:18 +0200

    Remove obsolete PHPDoc to not mess with ecphp/eu-login-bundle when doing static analysis.

--- src/Security/Core/User/CasUserProviderInterface.php
+++ src/Security/Core/User/CasUserProviderInterface.php
@@ -7,13 +7,7 @@ namespace EcPhp\CasBundle\Security\Core\User;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
-/**
- * Interface CasUserProviderInterface.
- */
 interface CasUserProviderInterface extends UserProviderInterface
 {
-    /**
-     * @return \EcPhp\CasBundle\Security\Core\User\CasUser
-     */
     public function loadUserByResponse(ResponseInterface $response): CasUserInterface;
 }
