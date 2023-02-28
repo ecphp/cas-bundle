4b4d065506673c12324e718ffe114d7059e117ae
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-02-28 19:16:08 +0100

    fix: use the Authenticator as entry point
    Let the Authenticator implements the `AuthenticationEntryPoint` interface.
    
    (cherry picked from commit d60de336b33d97991ccda3bfd8ab24995b36abd9)

--- src/Security/CasAuthenticator.php
+++ src/Security/CasAuthenticator.php
@@ -13,9 +13,11 @@ namespace EcPhp\CasBundle\Security;
 
 use EcPhp\CasBundle\Cas\SymfonyCasInterface;
 use EcPhp\CasBundle\Security\Core\User\CasUserProviderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -23,12 +25,16 @@ use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Throwable;
 
-final class CasAuthenticator extends AbstractAuthenticator
+final class CasAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
 {
-    public function __construct(private readonly SymfonyCasInterface $cas, private readonly CasUserProviderInterface $userProvider)
-    {
+    public function __construct(
+        private readonly SymfonyCasInterface $cas,
+        private readonly CasUserProviderInterface $userProvider,
+        private readonly UrlGeneratorInterface $urlGenerator
+    ) {
     }
 
     public function authenticate(Request $request): Passport
@@ -79,6 +85,24 @@ final class CasAuthenticator extends AbstractAuthenticator
         $request->overrideGlobals();
 
         return new RedirectResponse($request->getUri());
+    }
+
+    public function start(Request $request, ?AuthenticationException $authException = null): Response
+    {
+        if (true === $request->isXmlHttpRequest()) {
+            return new JsonResponse(
+                ['message' => 'Authentication required'],
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        return new RedirectResponse(
+            $this
+                ->urlGenerator
+                ->generate(
+                    'cas_bundle_login',
+                )
+        );
     }
 
     public function supports(Request $request): bool
